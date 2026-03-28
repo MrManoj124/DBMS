f8ebf316b2a5dd5674696b547b7976cc289fbe66
MrManoj124 <manojmanorooban@gmail.com>
2026-03-28 23:55:01 +0530

    create Customers table

--- DBMS_Basic/Basicfiles.sql
+++ DBMS_Basic/Basicfiles.sql
@@ -1,3 +1,10 @@
 #This is only for table and table constraints and queries
 CREATE DATABASE VehicleRentalDB;
 USE VehicleRentalDB;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(100),
+    Email VARCHAR(100) UNIQUE,
+    Phone VARCHAR(15)
+);
