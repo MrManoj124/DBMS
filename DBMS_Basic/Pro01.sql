3329ffe8584725ea8e87a51516ebac9ba61bc99d
MrManoj124 <manojmanorooban@gmail.com>
2026-03-29 14:24:32 +0530

    create Shipments table

--- DBMS_Basic/Pro01.sql
+++ DBMS_Basic/Pro01.sql
@@ -1,12 +1,12 @@
-create project database;
+create database project;
 use project;
 
 create table Suppliers(
     SID varchar(20) PRIMARY KEY,
     SName varchar(50) NOT NULL,
     Status INT,
-    SCity varchar(50)NOT NULL);
-)
+    SCity varchar(50)NOT NULL)
+);
 
 create table Parts(
     PID varchar(10) PRIMARY KEY,
@@ -14,11 +14,18 @@ create table Parts(
     Colour varchar(20) NOT NULL,
     Weight INT,
     PCity varchar(30), NOT NULL);
-)
+);
 
 create table Projects(
     PrID varchar(10) PRIMARY KEY,
     PrName varchar(30) NOT NULL,
     PrCity varchar(30) NOT NULL);
-)
+);
 
+create table Shipments(
+    SID varchar(10),
+    PID varchar(10),
+    PrID varchar(10),
+    Quantity INT,
+    
+);
