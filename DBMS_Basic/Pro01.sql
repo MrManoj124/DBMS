dc14b79f01436b56b6c326d1844bc1d4d38ab3b1
MrManoj124 <manojmanorooban@gmail.com>
2026-03-29 21:37:40 +0530

    Add values into Parts table

--- DBMS_Basic/Pro01.sql
+++ DBMS_Basic/Pro01.sql
@@ -40,4 +40,12 @@ create table Shipments(
         ON DELETE CASCADE
 );
 
+INSERT INTO Parts (PID, PName, Colour, Weight, PCity) VALUES
+('P1','Nut','Red',12,'London'),
+('P2','Bolt','Green',17,'Paris'),
+('P3','Screw','Blue',17,'Rome'),
+('P4','Screw','Red',14,'London'),
+('P5','Cam','Blue',12,'Paris'),
+('P6','Cog','Red',19,'London');
+
 
