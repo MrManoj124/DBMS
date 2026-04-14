5c6fc4e0b304a1c462658d06a6f7001237375903
MrManoj124 <manojmanorooban@gmail.com>
2026-04-14 21:28:36 +0530

    Add foreign key constraints for Appoinments table

--- DBMS_Basic/Hospital_DB.sql
+++ DBMS_Basic/Hospital_DB.sql
@@ -76,5 +76,7 @@ create table Appointments(
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     constraint pk_appointments PRIMARY KEY (appointment_id),
+    constraint fk_appoin_id FOREIGN KEY(patient_id) REFERENCES Patients(patient_id) ON DELETE RESTRICT,
+    constraint fk_appoin_doctor_id FOREIGN KEY(doctor_id) REFERENCES Doctors(doctor_id) ON DELETE RESTRICT,
     
 )
