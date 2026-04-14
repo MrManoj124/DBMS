0d121d2f28fa09979043ffeaeedb06c5072801e8
MrManoj124 <manojmanorooban@gmail.com>
2026-04-14 19:56:55 +0530

    Create Departments table

--- DBMS_Basic/Hospital_DB.sql
+++ DBMS_Basic/Hospital_DB.sql
@@ -2,3 +2,16 @@ create database if does not exist Hospital_db;
 
 use Hospital_db;
 
+--Create Departments table
+create table Departments(
+    department_id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(100) NOT NULL ,
+    dept_code CHAR(5) NOT NULL,
+    floor_number TINYINT NOT NULL,
+    phone_txt VARCHAR,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_departments PRIMARY KEY (department_id),
+    
+)
