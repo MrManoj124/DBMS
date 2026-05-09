f33fecc3319cf708f86a19cef2879b851ad826fa
MrManoj124 <manojmanorooban@gmail.com>
2026-05-09 23:32:59 +0530

    Add values for Job_details table

--- DBMS_Basic/Customer_Tab_Constrain.sql
+++ DBMS_Basic/Customer_Tab_Constrain.sql
@@ -50,3 +50,23 @@ INSERT INTO Location VALUES(1200, 'Anuradhapura');
 INSERT INTO Location VALUES(1300, 'Kandy');
 INSERT INTO Location VALUES(1400, 'Jaffna');
 
+
+INSERT INTO Job_details VALUES('AD_PRES', 'President', 20000.0, 40000.0);
+INSERT INTO Job_details VALUES('AD_VP', 'Administration Vice President', 15000.0, 30000.0);
+INSERT INTO Job_details VALUES('AD_ASST', 'Administration Assistant', 3000.0, 6000.0);
+INSERT INTO Job_details VALUES('FI_MGR', 'Finance Manager', 8200.0, 16000.0);
+INSERT INTO Job_details VALUES('FI_ACCOUNT', 'Accountant', 4200.0, 9000.0);
+INSERT INTO Job_details VALUES('AC_MGR', 'Accounting Manager', 8200.0, 16000.0);
+INSERT INTO Job_details VALUES('AC_ACCOUNT', 'Public Accountant', 4200.0, 9000.0);
+INSERT INTO Job_details VALUES('SA_MAN', 'Sales Manager', 10000.0, 20000.0);
+INSERT INTO Job_details VALUES('SA_REP', 'Sales Representative', 6000.0, 12000.0);
+INSERT INTO Job_details VALUES('PU_MAN', 'Purchasing Manager', 8000.0, 15000.0);
+INSERT INTO Job_details VALUES('PU_CLERK', 'Purchasing Clerk', 2500.0, 5500.0);
+INSERT INTO Job_details VALUES('ST_MAN', 'Stock Manager', 5500.0, 8500.0);
+INSERT INTO Job_details VALUES('ST_CLERK', 'Stock Clerk', 2000.0, 5000.0);
+INSERT INTO Job_details VALUES('SH_CLERK', 'Shipping Clerk', 2500.0, 5500.0);
+INSERT INTO Job_details VALUES('IT_PROG', 'Programmer', 4000.0, 10000.0);
+INSERT INTO Job_details VALUES('MK_MAN', 'Marketing Manager', 9000.0, 15000.0);
+INSERT INTO Job_details VALUES('MK_REP', 'Marketing Representative', 4000.0, 9000.0);
+INSERT INTO Job_details VALUES('HR_REP', 'Human Resources Representative', 4000.0, 9000.0);
+INSERT INTO Job_details VALUES('PR_REP', 'Public Relations Representative', 4500.0, 10500.0);
