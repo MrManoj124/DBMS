c8d8c6830da0fc01d28c5e97b8c9927915588105
MrManoj124 <manojmanorooban@gmail.com>
2026-05-09 23:30:05 +0530

    create Job_Details table

--- DBMS_Basic/Customer_Tab_Constrain.sql
+++ DBMS_Basic/Customer_Tab_Constrain.sql
@@ -6,3 +6,12 @@ CREATE TABLE Location (
     Location_ID INT PRIMARY KEY,
     City VARCHAR(50) NOT NULL DEFAULT 'Vavuniya'
 );
+
+
+CREATE TABLE Job_Details (
+    Job_ID VARCHAR(20) PRIMARY KEY,
+    Job_Title VARCHAR(100) NOT NULL,
+    Min_Salary DECIMAL(10,2) DEFAULT 5000 CHECK (Min_Salary > 0),
+    Max_Salary DECIMAL(10,2) DEFAULT 100000 CHECK (Max_Salary > 0)
+);
+
