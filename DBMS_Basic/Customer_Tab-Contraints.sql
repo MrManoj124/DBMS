6bdf81add0822c51ae652c8b443b6209ce966602
MrManoj124 <manojmanorooban@gmail.com>
2026-05-10 18:27:55 +0530

    create Cutomers table

--- DBMS_Basic/Customer_Tab-Contraints.sql
+++ DBMS_Basic/Customer_Tab-Contraints.sql
@@ -2,3 +2,11 @@ create database Customer_db;
 
 use Customer_db;
 
+CREATE TABLE CUSTOMERS (
+ID INT NOT NULL,
+NAME VARCHAR (20) NOT NULL,
+AGE INT NOT NULL,
+ADDRESS CHAR (25),
+SALARY DECIMAL (18, 2),
+PRIMARY KEY (ID)
+);
