46d1fbdaf93d449cbc5993d3c016d5e89104265c
MrManoj124 <manojmanorooban@gmail.com>
2026-05-11 01:13:52 +0530

    Add constraint to list down the constraint implemented on CUSTOMERS_3

--- DBMS_Basic/Customer_Tab-Contraints.sql
+++ DBMS_Basic/Customer_Tab-Contraints.sql
@@ -118,3 +118,7 @@ PRIMARY KEY(PID),
 CONSTRAINT Constraint_DOO CHECK(DATE_OF_ORDER <= '2023-
 02-09')
 );
+
+-- Execute the following statement to list down the constraint implemented on CUSTOMERS_3
+select table_name, constraint_type, constraint_name from information_schema.table_constraints where table_name='PRODUCTS';
+
