bdb5f586c2bc99acdc10bb38a1fddc02798a0bcd
MrManoj124 <manojmanorooban@gmail.com>
2026-05-11 08:57:10 +0530

    Add a syntax for Stored procedure

---
+++ DBMS_Basic/Stored_Procedure.sql
@@ -0,0 +1,11 @@
+-- Syntax
+DELIMITER $$
+CREATE PROCEDURE procedure_name (parameter_1, datatype_1, parameter_2, datatype_2, ...)
+BEGIN
+-- SQL statements to be executed
+END $$
+DELIMITER;
+
+To Execute the procedure;
+CALL <Procedure name>;
+
