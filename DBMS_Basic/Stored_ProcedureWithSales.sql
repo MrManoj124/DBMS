405db3718d333a40e70dc462106d450723cd2829
MrManoj124 <manojmanorooban@gmail.com>
2026-05-20 22:56:00 +0530

    create Customers table

--- DBMS_Basic/Stored_ProcedureWithSales.sql
+++ DBMS_Basic/Stored_ProcedureWithSales.sql
@@ -2,3 +2,11 @@ create database Sales_system;
 
 use Sales_system;
 
+-- create Customers Table
+create table Customers(
+    CustomerID int primary key,
+    Name varchar(50),
+    City varchar(50)
+);
+
+-- Products table
