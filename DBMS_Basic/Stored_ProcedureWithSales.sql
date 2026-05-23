104dc258e8c38ba12b53c8f5024ef4e075323fe6
MrManoj124 <manojmanorooban@gmail.com>
2026-05-23 22:52:07 +0530

    insert data into Products table

--- DBMS_Basic/Stored_ProcedureWithSales.sql
+++ DBMS_Basic/Stored_ProcedureWithSales.sql
@@ -57,3 +57,16 @@ INSERT INTO Customers(CustomerID, Name, City) VALUES
 (9,'Rohan','Galle'),
 (10,'Dilani','Colombo'),
 (11,'Ishara','Colombo');
+
+INSERT INTO Products(ProductID, ProductName, Price) VALUES
+(1,'Laptop',150000),
+(2,'Mouse',2000),
+(3,'Keyboard',5000),
+(4,'Monitor',30000),
+(5,'Printer',25000),
+(6,'Tablet',80000),
+(7,'Phone',120000),
+(8,'Speaker',7000),
+(9,'Camera',90000),
+(10,'Router',6000),
+(11,'SSD',20000);
