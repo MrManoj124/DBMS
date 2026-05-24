beabc25bc9518335bd518ee56c01b9974658cece
MrManoj124 <manojmanorooban@gmail.com>
2026-05-24 18:19:48 +0530

    create Products table

--- DBMS_Basic/Triggers.sql
+++ DBMS_Basic/Triggers.sql
@@ -8,3 +8,12 @@ CREATE TABLE Customers (
     Phone VARCHAR(15),
     City VARCHAR(50)
 );
+
+
+create table Products(
+ ProductID INT primary key auto_increment,
+ ProductName VARCHAR(100) NOT NULL,
+ Category VARCHAR(50),
+ Price DECIMAL (10, 2) NOT NULL CHECK (Price > 0),
+ StockQuantity int not null check (StockQuantity >= 0)
+);
