65f702d2837dd0edbafa8bbe422b35d9a76bdaa0
MrManoj124 <manojmanorooban@gmail.com>
2026-05-24 18:19:30 +0530

    create Customers table

--- DBMS_Basic/Triggers.sql
+++ DBMS_Basic/Triggers.sql
@@ -1,3 +1,10 @@
 create database Backery;
 
 use Backery;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY AUTO_INCREMENT,
+    CustomerName VARCHAR(100) NOT NULL,
+    Phone VARCHAR(15),
+    City VARCHAR(50)
+);
