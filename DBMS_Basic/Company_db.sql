7647153957ad14a60c2a5912ccfed17d7692bef6
MrManoj124 <manojmanorooban@gmail.com>
2026-06-06 23:53:22 +0530

    create Suppliers Table

--- DBMS_Basic/Company_db.sql
+++ DBMS_Basic/Company_db.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE SPJ_Database;
 USE SPJ_Database;
+
+-- create Suppliers Table
+CREATE TABLE Suppliers (
+    SID VARCHAR(5) PRIMARY KEY,
+    SName VARCHAR(50) NOT NULL,
+    Status INT,
+    SCity VARCHAR(50)
+);
