aab43d5fd522a2060930dec46599508b7d0eed4f
MrManoj124 <manojmanorooban@gmail.com>
2026-06-11 23:45:06 +0530

    feat : Create database and use it

--- DBMS_Basic/Mock_Exam.sql
+++ DBMS_Basic/Mock_Exam.sql
@@ -1,1 +1,11 @@
--- 
+-- This file shows an exam preparation paper
+-- create a database
+-- 01. Vehicle Rental System
+create database Vehicle_Rental;
+
+-- Use the Created database
+use Vehicle_Rental;
+
+-- create table for the database
+-- create a Branch table
+create table 
